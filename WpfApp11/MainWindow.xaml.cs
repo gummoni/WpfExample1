3ad155e53019a8e111f53dcb95b144a6bf0736fd
gummo <shaty3@hotmail.com>
2023-02-21 23:56:35 +0900

    プロジェクト ファイルを追加します。

---
+++ WpfApp11/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp11
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new MainWindowViewModel();
+        }
+
+    }
+
+    public class MainWindowViewModel
+    {
+        public MainWindowViewModel()
+        {
+            this.OkCommand = new OkCommandImpl();
+        }
+
+        public ICommand OkCommand { get; private set; }
+    }
+
+    public class OkCommandImpl : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            MessageBox.Show("OKボタンが押されました。");
+        }
+    }
+}
